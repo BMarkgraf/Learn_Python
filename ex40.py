46456f234ed80cadd5d2d456a9c3b2435d23b2a7
Brandon Markgraf <brandon.markgraf@target.com>
2019-03-29 11:49:54 -0500

    finished ex40

---
+++ ex40.py
@@ -0,0 +1,31 @@
+class Song(object):
+  def __init__(self, lyrics):
+      self.lyrics = lyrics
+  def sing_me_a_song(self):
+    for line in self.lyrics:
+      print (line)
+
+happy_bday = Song([
+  "Happy birthday to you",
+  "I don't want to get sued",
+  "So I'll stop right there"
+])
+
+bulls_on_parade = Song([
+  "They rally around the family",
+  "With pockets full of shells"
+])
+
+test_song = [
+  "This is line 1",
+  "Followed by line 2",
+  "Lastly, line 3!!!"
+]
+
+test = Song(test_song)
+
+test.sing_me_a_song()
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
